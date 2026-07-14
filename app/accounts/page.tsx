e68ee9bad07e5cfc19ab11025c1a63b653d4fc56
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-13 18:56:39 -0700

    Finished the initial design on the accounts page

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -1,22 +1,37 @@
+"use client"
+import { useState } from "react";
 import Button from "../components/button";
 import Card from "../components/card";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import LabeledInput from "../components/labeled-input";
+import { useRouter } from "next/navigation";
 
 export default function AccountsPage() {
+  const router = useRouter();
+  const [isAccountCreationEnabled, setIsAccountCreationEnabled] = useState<boolean>(false);
+  const toggleAccountCreation = () => setIsAccountCreationEnabled(!isAccountCreationEnabled);
+
+  function navigateToHome() {
+    router.push('/');
+  }
+
   return (
     <main className="w-full h-full">
       <Header />
       <div className="w-full h-[calc(100%-10rem)] p-8">
         <Card>
-            <h2 className="text-5xl">Sign In</h2>
+            <h2 className="text-5xl">{isAccountCreationEnabled ? "Create Account" : "Sign In"}</h2>
             <LabeledInput id="userName" label="Username" inputType="text" />
             <LabeledInput id="passWord" label="Password" inputType="password" />
+            {isAccountCreationEnabled && <LabeledInput id="passWord" label="Confirm Password" inputType="password" />}
 
-            <Button>Login</Button>
+            {!isAccountCreationEnabled && <Button onClick={navigateToHome}>Login</Button>}
+            {!isAccountCreationEnabled && <p>New User? <span className="text-blue-600 cursor-pointer select-none" onClick={toggleAccountCreation}>Create New Account</span></p>}
 
-            <p>New User? <span className="text-blue-600 cursor-pointer select-none">Create New Account</span></p>
+            {isAccountCreationEnabled && <Button>Create Account</Button>}
+            {isAccountCreationEnabled && <Button type="warning" onClick={toggleAccountCreation}>Cancel</Button>}
+
         </Card>
       </div>
       <Footer />
--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,9 +1,19 @@
 interface ButtonProps {
     children: React.ReactNode;
+    type?: "default" | "warning"
+    onClick?: () => void
 }
 
-export default function Button({children}: ButtonProps) {
+export default function Button({children, type, onClick}: ButtonProps) {
+    let bgColor: string = "bg-graphite-800";
+    let bgColorHover: string = "hover:bg-cool-sky-300/20";
+
+    if (type === "warning") {
+        bgColor = "bg-red-500"
+        bgColorHover = "hover:bg-red-500/80"
+    }
+
     return (
-        <button className="w-1/2 h-12 text-2xl rounded-2xl bg-graphite-800 cursor-pointer hover:bg-cool-sky-300/20">{children}</button>
+        <button className={`w-1/2 h-12 text-2xl rounded-2xl cursor-pointer ${bgColor} ${bgColorHover}`} onClick={onClick}>{children}</button>
     )
 }
