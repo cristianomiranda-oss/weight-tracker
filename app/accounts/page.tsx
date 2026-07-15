e63878b8e0b8c83b250946da7627660553e4f152
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-15 16:59:40 -0700

    Cleaned up the hidden elements on the page

--- app/accounts/page.tsx
+++ app/accounts/page.tsx
@@ -22,16 +22,20 @@ export default function AccountsPage() {
       <div className="w-full h-[calc(100%-10rem)] p-8">
         <Card>
             <h2 className="text-5xl">{isAccountCreationEnabled ? "Create Account" : "Sign In"}</h2>
+            
             <LabeledInput id="userName" label="Username" inputType="text" />
             <LabeledInput id="passWord" label="Password" inputType="password" />
             {isAccountCreationEnabled && <LabeledInput id="passWord" label="Confirm Password" inputType="password" />}
 
-            {!isAccountCreationEnabled && <Button onClick={navigateToHome}>Login</Button>}
-            {!isAccountCreationEnabled && <p>New User? <span className="text-blue-600 cursor-pointer select-none" onClick={toggleAccountCreation}>Create New Account</span></p>}
+            {!isAccountCreationEnabled && <>
+              <Button onClick={navigateToHome}>Login</Button>
+              <p>New User? <span className="text-blue-600 cursor-pointer select-none" onClick={toggleAccountCreation}>Create New Account</span></p>
+            </>}
 
-            {isAccountCreationEnabled && <Button>Create Account</Button>}
-            {isAccountCreationEnabled && <Button type="warning" onClick={toggleAccountCreation}>Cancel</Button>}
-
+            {isAccountCreationEnabled && <>
+              <Button>Create Account</Button>
+              <Button type="warning" onClick={toggleAccountCreation}>Cancel</Button>
+            </>}
         </Card>
       </div>
       <Footer />
