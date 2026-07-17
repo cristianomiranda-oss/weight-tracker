a4a5058529b2c7bb753f5bc36fb9f29ab2555cbc
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-16 17:00:21 -0700

    Updated the weight display and added breakpoint changes

--- app/components/weight-entry.tsx
+++ app/components/weight-entry.tsx
@@ -7,10 +7,10 @@ interface WeightEntryProps {
 
 export default function WeightEntry({ weightEntryObj, removeEntry }: WeightEntryProps) {
     return (
-        <div className="w-full h-8 text-xl flex justify-around items-center bg-dusty-taupe-600">
-            <p className="w-1/3 text-center">{weightEntryObj.weightDate.getDate()}</p>
-            <p className="w-1/3 text-center">{weightEntryObj.weightValue}</p>
-            <p className="w-1/3 text-center" onClick={removeEntry}>X</p>
+        <div className="w-full h-8 md:h-10 text-xl md:text-3xl flex justify-around items-center bg-dusty-taupe-600">
+            <p className="w-5/12 text-center">{weightEntryObj.weightDate.getDate()}</p>
+            <p className="w-5/12 text-center">{weightEntryObj.weightValue}</p>
+            <p className="w-2/12 text-center" onClick={removeEntry}>X</p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,10 @@ export default function WeightLogHome() {
       <div className="w-full h-[calc(100%-10rem)] p-8">
         <Card className="p-0">
           <div className="w-full h-full flex flex-col bg-dusty-taupe-500 overflow-y-scroll scrollbar-track-dusty-taupe-700 scrollbar-thumb-turf-green-600">
-            <div className="w-full h-10 sticky top-0 flex justify-around items-center border-b-2 text-3xl bg-dusty-taupe-700">
-              <h2 className="w-1/3 text-center">Date</h2>
-              <h2 className="w-1/3 text-center">Weight</h2>
-              <h2 className="w-1/3 text-center">X</h2>
+            <div className="w-full h-10 md:h-12 sticky top-0 flex justify-center items-center border-b-2 text-3xl md:text-4xl bg-dusty-taupe-700">
+              <h2 className="w-5/12 text-center">Date</h2>
+              <h2 className="w-5/12 text-center">Weight</h2>
+              <h2 className="w-2/12 text-center">X</h2>
             </div>
 
             {tempWeightArr.map(entry => <WeightEntry key={entry.WeightEntryId} weightEntryObj={entry} removeEntry={() => removeWeightEntry(entry.WeightEntryId, entry.userId)} />)}
