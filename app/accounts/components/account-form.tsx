d68ad231681e6616c11d2e43f2e05470b87ad9e7
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-18 16:41:48 -0700

    Updated documentation for the account form component

--- app/accounts/components/account-form.tsx
+++ app/accounts/components/account-form.tsx
@@ -15,20 +15,34 @@ interface AccountFormProps {
   ) => Promise<void>;
 }
 
+/**
+     * Contains the components for displaying the account sign in and account creation interface and
+     * handles interactions made with the interface. 
+     */
 export default function AccountForm({
   validateLogin,
   createUserAccount,
-}: AccountFormProps) {
+}: AccountFormProps): React.JSX.Element {
+  // Initializes a router to allow for navigation
   const router = useRouter();
+
+  // Initializes a state flag for denoting which screen is active
   const [isAccountCreationEnabled, setIsAccountCreationEnabled] =
     useState<boolean>(false);
+
+  // Initializes state for storing the loading flag and error messages 
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  // Initializes various references to the html input elements in the form
   const userNameRef = useRef<HTMLInputElement | null>(null);
   const userPasswordRef = useRef<HTMLInputElement | null>(null);
   const confirmPassWordRef = useRef<HTMLInputElement | null>(null);
 
+  /**
+     * Changes the current screen to reveal the necessary elements for either the account sign in or account creation screens.
+     * Change does not occur if any function call is already loading.
+     */
   function toggleAccountCreation() {
     // Only swaps screens if no calls are loading
     if (!isLoading) {
@@ -36,6 +50,11 @@ export default function AccountForm({
     }
   }
 
+  /**
+     * Handles the submission of the form based on the current active screen.
+     *
+     * @param e The data associated with the submit event
+     */
   async function submitForm(e: React.SubmitEvent<HTMLFormElement>) {
     try {
       // Prevents the form submission event
@@ -45,20 +64,22 @@ export default function AccountForm({
       setIsLoading(true);
       setErrorMessage("");
 
-      // Checks that userName and userPassword references to associated text inputs are established
-      if (isAccountCreationEnabled) {
+      // Checks the currently active screen
+      if (isAccountCreationEnabled) { // Account Creation Screen
         // Checks that confirmPassword ref to associated text input is established
         if (
           userNameRef.current !== null &&
           userPasswordRef.current !== null &&
           confirmPassWordRef.current !== null
         ) {
+          // Calls the user account creation method
           await createUserAccount(
             userNameRef.current.value,
             userPasswordRef.current.value,
             confirmPassWordRef.current.value,
           );
 
+          // Toggles the current screen to change to the sign in screen
           toggleAccountCreation();
 
           // Clears text inputs
@@ -66,12 +87,17 @@ export default function AccountForm({
           userPasswordRef.current.value = "";
           confirmPassWordRef.current.value = "";
         }
-      } else {
+      } else { // Account Sign In Screen
+        // Checks that the username and password input refs are established
         if (userNameRef.current !== null && userPasswordRef.current !== null) {
+          // Calls the method to validate the user's login
+          // If no errors are thrown the function continues
           await validateLogin(
             userNameRef.current.value,
             userPasswordRef.current.value,
           );
+
+          // Navigates to the home page
           router.push("/");
 
           // Clears text inputs
@@ -80,12 +106,14 @@ export default function AccountForm({
         }
       }
     } catch (error) {
+      // Checks if the error is an established error
       if (error instanceof Error) {
         setErrorMessage(error.message);
-      } else {
+      } else { // Indicates an unusual error has occurred
         setErrorMessage("An Unknown Error has Occurred!");
       }
     } finally {
+      // Changes the loading boolean to indicate the function is no longer running.
       setIsLoading(false);
     }
   }
