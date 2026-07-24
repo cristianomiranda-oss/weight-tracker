7c166f707e0f4b0708dd1ca04134b0d50576c85e
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-24 16:09:46 -0700

    Began working on the sorting selection design

--- app/components/weight-log-table.tsx
+++ app/components/weight-log-table.tsx
@@ -1,4 +1,5 @@
 import type { sortOrder, WeightEntryType } from "../libs/types";
+import Button from "./button";
 import WeightEntry from "./weight-entry";
 
 interface WeightLogTableProps {
@@ -20,8 +21,9 @@ export default function WeightLogTable({
   return (
     <div className="w-full h-full min-h-46 flex flex-col bg-dusty-taupe-500 overflow-y-scroll scrollbar-track-dusty-taupe-700 scrollbar-thumb-turf-green-600">
       <div className="w-full h-12 min-h-12 md:h-16 md:min-h-16 sticky top-0 flex justify-center items-center border-b-2 text-3xl md:text-4xl bg-dusty-taupe-700">
-        <div className="w-6/12">
+        <div className="w-6/12 flex flex-row justify-center gap-6">
           <h2 className="text-center">Date</h2>
+          <Button className="rotate-90">{'<'}</Button>
         </div>
         <h2 className="w-5/12 text-center">Weight</h2>
         <h2 className="w-1/12 text-center">X</h2>
