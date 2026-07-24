b52603813994fd83a93e5df24a759d2c69b71491
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-24 15:40:19 -0700

    Fixed more css issues on the weight log home page

--- app/components/weight-log-table.tsx
+++ app/components/weight-log-table.tsx
@@ -16,8 +16,8 @@ export default function WeightLogTable({
   triggerEntryUpdate,
 }: WeightLogTableProps) {
   return (
-    <div className="w-full h-full min-h-46 max-h-full flex flex-col bg-dusty-taupe-500 overflow-y-scroll scrollbar-track-dusty-taupe-700 scrollbar-thumb-turf-green-600">
-      <div className="w-full h-10 md:h-12 sticky top-0 flex justify-center items-center border-b-2 text-3xl md:text-4xl bg-dusty-taupe-700">
+    <div className="w-full h-full min-h-46 flex flex-col bg-dusty-taupe-500 overflow-y-scroll scrollbar-track-dusty-taupe-700 scrollbar-thumb-turf-green-600">
+      <div className="w-full h-12 min-h-12 md:h-16 md:min-h-16 sticky top-0 flex justify-center items-center border-b-2 text-3xl md:text-4xl bg-dusty-taupe-700">
         <h2 className="w-6/12 text-center">Date</h2>
         <h2 className="w-5/12 text-center">Weight</h2>
         <h2 className="w-1/12 text-center">X</h2>
