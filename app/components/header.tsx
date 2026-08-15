7fc0786382e27b1d1f96253b9959360c8ad4890d
cristiano-oss <cristiano.mirande@snhu.edu>
2026-08-14 20:27:16 -0700

    fixed CSS issue

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 export default function Header({ children }: HeaderProps) {
   return (
     <header className="w-full h-20 bg-header-footer px-3 py-0.5 flex justify-center lg:justify-between items-center">
-      <h1 className="text-6xl">Weight Tracker</h1>
+      <h1 className="text-4xl md:text-6xl">Weight Tracker</h1>
       {children}
     </header>
   );
